bdd6b13ff2a8b303275dfe70a9050954e3041198
Zhang Ximin <zhangximin@gmail.com>
2016-11-11 16:28:07 +0800

    add google pagespeed.

--- inst_nginx_2.1.1.sh
+++ inst_nginx_2.1.1.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-sudo apt-get install libxml2-dev libcurl4-openssl-dev libapr1-dev libmcrypt-dev
+sudo apt-get install libxml2-dev libcurl4-openssl-dev libapr1-dev libmcrypt-dev libjpeg62-dev
+
+# google PageSpeed for nginx.
+sudo apt-get install build-essential zlib1g-dev libpcre3 libpcre3-dev unzip
+NPS_VERSION=1.11.33.3
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -O release-${NPS_VERSION}-beta.zip
+unzip release-${NPS_VERSION}-beta.zip
+cd ngx_pagespeed-release-${NPS_VERSION}-beta/
+wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
+tar -xzvf ${NPS_VERSION}.tar.gz  # extracts to psol/
 
 # make tengine
+TENGIN_VERSION=2.1.1
 wget -c http://tengine.taobao.org/download/tengine-2.1.1.tar.gz
-if [ -f "./tengine-2.1.1.tar.gz" ];then
-
-tar zxf tengine-2.1.1.tar.gz
-cd tengine-2.1.1
+tar zxf tengine-${TENGIN_VERSION}.tar.gz
+cd tengine-${TENGIN_VERSION}
 ./configure --prefix=/usr/local/nginx \
 --user=www-data \
 --group=www-data \
@@ -15,7 +23,8 @@ cd tengine-2.1.1
 --without-http-cache \
 --with-http_ssl_module \
 --with-http_gzip_static_module \
---with-http_concat_module
+--with-http_concat_module \
+--add-module=../ngx_pagespeed-release-${NPS_VERSION}-beta
 CPU_NUM=$(cat /proc/cpuinfo | grep processor | wc -l)
 if [ $CPU_NUM -gt 1 ];then
     make -j$CPU_NUM
