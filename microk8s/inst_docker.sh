ed381f944d4e42b133dedfc774bcf01782da1db5
Simon Cheung <simon@tiertime.net>
2021-01-01 15:15:48 +0800

    temp commit.

--- microk8s/inst_docker.sh
+++ microk8s/inst_docker.sh
@@ -67,6 +67,14 @@ echo ""
 #############################################################
 echo "Adding ${USER} to group docker."
 sudo usermod -aG docker ${USER}
+
+#echo "Installing docker-runner"
+#sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+#sudo chmod +x /usr/local/bin/gitlab-runner
+#sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+#sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+#sudo gitlab-runner start
+
 echo "Done."
 echo ""
 echo ""
